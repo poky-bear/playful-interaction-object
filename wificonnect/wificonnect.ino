71fdc3d2f16c72d4c3d67d300e0945f7646f521c
Sara <helenyuzhang@gmail.com>
2025-04-22 23:21:08 -0400

    establish wifi connection

--- wificonnect/wificonnect.ino
+++ wificonnect/wificonnect.ino
@@ -1,9 +1,36 @@
+#include "WiFi.h"
+#include "Secret.h"
+
+
+
+WiFiUDP udp;
+const char *udpAddress = "192.168.1.2";
+const int udpSenderPort = 5006;
+const int udpReceiverPort = 5005;
+
+String sentMessage;
+String receivedMessage;
+
 void setup() {
-  // put your setup code here, to run once:
+  // (Optional)Press reset button
+  // on the dev board to see these print statements
+
+  Serial.begin(115200);
+  initUDP();
+}
+
+void initUDP() {
+  WiFi.begin(ssid, pass);
+  Serial.println("Connecting to wifi");
+
+  while(WiFi.status() != WL_CONNECTED){
+    delay(1000);
+    Serial.print(".");
+  }
+
+  udp.begin(udpReceiverPort);
+  Serial.println("UDP Initialized");
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+void loop() {}
